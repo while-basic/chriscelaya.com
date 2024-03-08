b6438099b684fad856f1c514ad0c4458473620e5
while-basic <c_celaya@icloud.com>
2024-03-08 05:11:18 -0700

    updates

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,20 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'template',
+    subtitle: '2023 template',
+    link: 'https://google.com',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2015-11-20',
+    desc:
+      'Built for a templates impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
     title: 'draft',
-    subtitle: 'subtitle',
+    subtitle: '2023 Project',
     link: 'https://google.com',
     image: '/images/projects/nearestdollar.jpg',
     date: '2015-11-20',
@@ -15,6 +27,7 @@ const data = [
   {
     title: 'Nearest Dollar',
     subtitle: '2015 BVP Hackathon',
+    link: '',
     image: '/images/projects/nearestdollar.jpg',
     date: '2015-11-20',
     desc:
@@ -33,27 +46,6 @@ const data = [
       'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
       + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
       + 'and nutrient deficiencies at an affordable price for the developing world.',
-  },
-  {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
-    desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
-  },
-  {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
   },
 ];
 
