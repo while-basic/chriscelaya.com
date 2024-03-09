3ca4e149ea115d903d31762ba829753df9bccb34
while-basic <c_celaya@icloud.com>
2024-03-09 15:53:30 -0700

    added cspell words, updated experience

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -1,48 +1,105 @@
-/**
- * @typedef {Object} Position
- * Conforms to https://jsonresume.org/schema/
- *
- * @property {string} name - Name of the company
- * @property {string} position - Position title
- * @property {string} url - Company website
- * @property {string} startDate - Start date of the position in YYYY-MM-DD format
- * @property {string|undefined} endDate - End date of the position in YYYY-MM-DD format.
- * If undefined, the position is still active.
- * @property {string|undefined} summary - html/markdown summary of the position
- * @property {string[]} highlights - plain text highlights of the position (bulleted list)
- */
 const work = [
   {
     name: 'T5 Data Centers',
     position: 'Data Center Technician II',
-    url: 'https://smileidentity.com',
+    url: 'https://t5datacenters.com/',
     startDate: '2021-08-01',
-    summary: `Smile builds machine learning APIs to compare user submitted photos to ID documents. Our APIs are
-    used by hundreds of thousands of people every day to access financial services. As VP of Engineering,
-    I provide technical leadership on high-impact projects, influence and coach a distributed team of engineers,
-    and facilitate alignment and clarity across teams on goals, outcomes, and timelines. I was promoted
-    from Director of Engineering to VP of Engineering in April 2022, and then to VP of Engineering and Head
-    of AI in November 2022. I lead a 20+ person engineering org. I directly manage ~8 engineers and spend
-    >50% of my time writing code.`,
+    summary: 'At T5 Data Centers, I specialized in maintaining and operating both critical and non-critical infrastructure, equipment, and systems. '
+              + 'My role involved close collaboration with a team of Critical Facilities Technicians (CFT) to guarantee total system availability (100% uptime) and minimize operational risks. '
+              + 'I proficiently utilized a Computerized Maintenance Management System (CMMS) for the efficient management of both scheduled and unscheduled data center tasks. '
+              + 'My expertise extended to conducting preventive maintenance on a wide range of electrical and mechanical equipment, ensuring their optimal performance and reliability. '
+              + 'I adeptly responded to emergencies and critical situations, either independently or as part of a team, with a focus on achieving complete recovery. '
+              + 'Troubleshooting and documenting technical problems were among my key skills, and I was capable of escalating issues when necessary to ensure their resolution. '
+              + 'I also supervised external contractors, ensuring they adhered to critical facility work rules, and provided essential on-call emergency support when required.'
+              + 'My portfolio showcases my commitment to excellence, problem-solving skills, and ability to maintain high standards of data center operation and management.',
     highlights: [
-      'Redesigned engineering processes for bug tracking, meetings, and standups. Improved culture for code reviews, blameless post-mortems, and retrospectives.',
-      'Re-architected engineering hiring and onboarding processes. Recruited several strong engineers.',
-      'Reorged engineering teams to focus on product delivery. Created a new team to focus on ML infrastructure.',
-      'Lead re-design of internal APIs for inference. Built new computer vision pipelines for industry leading certifications (NIST/iBeta liveness).',
-      'Pitched, designed, developed, deployed, and maintain a fraud detection product based on 1-N facial recognition using embeddings and vector search.',
+      'Specialized in the maintenance and operation of both critical and non-critical data center infrastructure, equipment, and systems, ensuring operational excellence.',
+      'Played a key role in a team of Critical Facilities Technicians (CFT) to achieve 100% system uptime, demonstrating exceptional teamwork and problem-solving abilities.',
+      'Proficiently managed both scheduled and unscheduled data center tasks using a Computerized Maintenance Management System (CMMS), showcasing strong organizational skills.',
+      'Conducted preventive maintenance on a broad spectrum of electrical and mechanical equipment, ensuring their optimal performance and reliability.',
+      'Responded to emergencies and critical situations effectively, either independently or as part of a team, emphasizing quick thinking and resilience.',
+      'Troubleshot and documented technical problems, capable of escalating issues when necessary to ensure swift resolution, highlighting excellent technical and communication skills.',
+      'Supervised external contractors to ensure adherence to critical facility work rules, demonstrating strong leadership and management capabilities.',
+      'Provided essential on-call emergency support, showcasing a commitment to operational reliability and customer service.',
     ],
   },
   {
-    name: 'Skeptical Investments',
-    position: 'Co-founder',
-    url: 'http://skepticalinvestments.biz',
-    startDate: '2017-04-01',
-    endDate: '2017-04-01',
-    summary: `Skeptical Investments is a micro-vc fund focused on Angel, Seed, and Series-A
-    YC companies.`,
+    name: 'CN Wire',
+    position: 'Mechatronics Technician',
+    url: 'https://www.cnwire.com/',
+    startDate: '2018-01-01',
+    endDate: '2021-08-01',
+    summary: 'At CN Wire, I performed troubleshooting, maintenance, and executed electrical repairs on factory equipment, leveraging my exceptional ability to interpret diagrams, schematics, and electrical code specifications.'
+              + 'I installed and repaired electrical commutators, electronic sensors, and Human Machine Interface (HMI) touch panels, ensuring seamless operation throughout the facility.'
+              + 'My role also involved maintaining various cooling, compression, and emulsion systems critical for the facility\'s continuous operation.'
+              + 'I took charge of updating manufacturing records, processing work orders, and drafting installation reports, demonstrating my meticulous attention to detail.'
+              + 'Moreover, I prepared technical documentation outlining common maintenance procedures and routines, serving as a crucial resource for the maintenance team.',
     highlights: [
-      'Created the InstaSafe. Fast, automated investment docs for YC companies.',
-      'Advise several startups in the fund.',
+      'Performed complex troubleshooting, maintenance, and electrical repairs on factory equipment at CN Wire, with a deep understanding of diagrams, schematics, and electrical codes.',
+      'Installed and repaired critical components including electrical commutators, electronic sensors, and HMI touch panels, ensuring optimal facility operations.',
+      'Maintained essential cooling, compression, and emulsion systems, vital for the facility\'s uninterrupted operation.',
+      'Oversaw the updating of manufacturing records, work order processing, and the creation of detailed installation reports, showcasing meticulous attention to detail.',
+      'Developed technical documentation for maintenance procedures and routines, becoming a key resource for the maintenance team.',
+      'Demonstrated expertise in installing, maintaining, and troubleshooting electrical components and equipment for industrial settings.',
+      'Managed end-to-end electrical projects, significantly reducing downtime and maintaining high standards of safety and quality.',
+      'Provided on-call emergency support, effectively resolving urgent issues as they occurred.',
+      'Worked closely with management to develop and implement corrective actions, significantly improving the production system\'s quality and safety.',
+    ],
+  },
+  {
+    name: 'Schneider Electric',
+    position: 'Wiring Technician, Quality Assurance',
+    url: 'https://www.schneiderelectric.com/',
+    startDate: '2016-01-01',
+    endDate: '2017-01-01',
+    summary: 'At Schneider Electric, I specialized in troubleshooting electrical and electromechanical operations, leveraging my exceptional ability to interpret diagrams, schematics, and electrical code specifications.'
+              + 'I installed and repaired electrical commutators, electronic sensors, and Human Machine Interface (HMI) touch panels, ensuring seamless operation throughout the facility.'
+              + 'My role also involved maintaining various cooling, compression, and emulsion systems critical for the facility\'s continuous operation.'
+              + 'I took charge of updating manufacturing records, processing work orders, and drafting installation reports, demonstrating my meticulous attention to detail.'
+              + 'Moreover, I prepared technical documentation outlining common maintenance procedures and routines, serving as a crucial resource for the maintenance team.',
+    highlights: [
+      'Troubleshooting Expertise: Specialized in troubleshooting electrical and electromechanical operations to ensure optimal performance and reliability.',
+      'Technical Interpretation: Proficient in interpreting point-to-point diagrams, bills of materials, and wiring schematics, enhancing project accuracy and execution.',
+      'Collaborative Problem-Solving: Worked closely with engineering and assembly teams to swiftly resolve problems, ensuring project continuity and success.',
+      'Advanced Diagnostic Skills: Utilized various tools and software for effective ground fault detection, preventing potential hazards and ensuring safety.',
+      'Quality Assurance: Oversaw testing and assembly of low voltage electronic products for large clients, guaranteeing high-quality deliverables.',
+    ],
+  },
+  // The Self Employed section was duplicated, removing the duplicate
+  {
+    name: 'Delfingen',
+    position: 'Maintenance Technician',
+    url: 'https://www.delfingen.com/',
+    startDate: '2015-01-01',
+    endDate: '2016-01-12',
+    summary: 'Industrial Maintenance Management: Managed maintenance on powered industrial equipment, including pneumatic, hydraulic, water, and electrical systems, to ensure uninterrupted operations.'
+              + 'Technical Proficiency: Executed tasks such as PVC cutter repair, electrical troubleshooting, and preventative maintenance, minimizing downtime and enhancing efficiency.'
+              + 'Problem Resolution: Identified and resolved issues with corrugated screws, cutters, pullers, and printing systems, ensuring machinery longevity and reliability.'
+              + 'Versatile Skill Set: Demonstrated versatility with additional duties including welding, fabrication, and building maintenance, showcasing a broad technical skill set.',
+    highlights: [
+      'Managed and maintained powered industrial equipment, including systems powered by pneumatic, hydraulic, water, and electrical sources.',
+      'Conducted PVC cutter repair, electrical troubleshooting, and preventative maintenance, ensuring equipment reliability and longevity.',
+      'Identified and resolved issues with corrugated screws, cutters, pullers, and printing systems, maintaining high operational standards.',
+      'Performed welding, fabrication, and building maintenance tasks, showcasing versatility and technical skill in maintenance operations.',
+    ],
+  },
+  {
+    name: 'DalTile',
+    position: 'Maintenance Technician III',
+    url: 'https://www.daltile.com/',
+    startDate: '2015-01-01',
+    endDate: '2016-01-12',
+    summary: 'At Dal-Tile, my role as a Maintenance Technician III involved the installation and maintenance of a diverse array of hydraulic presses, electrical panels, and mechanical components, essential for the company\'s production line. '
+              + 'My responsibilities included swiftly acknowledging and rectifying machine malfunctions, installing electro-mechanical valves, and conducting routine maintenance to minimize production downtime. '
+              + 'Additionally, I was tasked with the maintenance of raw material conveyors, dust, compression, and cooling systems, ensuring the smooth operation of the manufacturing process. '
+              + 'My hands-on experience extended to operating industrial hydraulic systems, performing oil changes, and executing routine die-block quality checks to uphold the highest standards of operational excellence. '
+              + 'Furthermore, I oversaw the management of materials processing and storage, utilizing a variety of conveying systems for efficient material transportation. '
+              + 'This role at Dal-Tile not only sharpened my technical skills but also reinforced my commitment to maintaining the highest quality standards in industrial maintenance and operations.',
+    highlights: [
+      'Installation and Maintenance: Installed and maintained hydraulic presses, electrical panels, and mechanical components, ensuring robust operations.',
+      'Diagnostic Expertise: Quickly acknowledged machine malfunctions, installing electro-mechanical valves, and performed routine maintenance to prevent operational disruptions.',
+      'System Maintenance: Conducted routine maintenance on raw material conveyors, dust, compression, and cooling systems, maintaining system integrity.',
+      'Operational Excellence: Managed materials processing and storage, monitored material transportation systems, and ensured routine quality checks, optimizing production processes.',
     ],
   },
 ];
