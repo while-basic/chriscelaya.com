118d6a97f0a8847a57730ea9728c313d6d40523d
while-basic <c_celaya@icloud.com>
2024-03-09 16:43:06 -0700

    updated About myself section

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Main from '../layouts/Main';
 
 const Index = () => (
-  <Main
-    description={"Christopher Celaya's personal website. El Paso based,"}
-  >
+  <Main description="Christopher Celaya's personal website. El Paso based">
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2><Link to="/">About this site</Link></h2>
-          <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
-          </p>
+          <h2><Link to="/">About Myself</Link></h2>
+          <p> </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
+      <p>
+        I am Christopher Celaya, a results-driven and innovative engineer deeply immersed in the
+        fascinating world of computer science, artificial intelligence, and mechatronics. Based in
+        the vibrant city of El Paso, Texas, I have dedicated my career to blending the meticulous
+        world of software development with the dynamic realm of industrial technology. With
+        extensive experience under my belt, I have had the privilege of contributing to cutting-edge
+        projects and tackling challenging roles, from a Data Center Technician at T5 Data Centers
+        to a hands-on Mechatronics Technician at CN Wire.
       </p>
-      <p> Source available <a href="https://github.com/while-basic">here</a>.</p>
+      <p>
+        My journey in technology began with a passion for understanding how things work, leading me
+        to explore various fields of engineering and programming. I have attended courses in
+        Electronics Engineering Technology and dabbled in Electrical Engineering & Computer
+        Science, always eager to expand my knowledge and apply it to real-world challenges. Whether
+        it is developing software solutions or maintaining and repairing industrial machinery, my
+        goal has always been to enhance efficiency, reliability, and safety in every project I
+        undertake.
+      </p>
+      <p>
+        As a self-taught programmer proficient in languages like C, Python, and JavaScript, I have
+        developed a knack for creating innovative solutions that bridge the gap between digital and
+        physical worlds. My hands-on experience with pneumatic, hydraulic, water, and electrical
+        systems has equipped me with a versatile skill set, enabling me to manage and maintain
+        complex systems with ease. Beyond technical skills, I value communication, collaboration,
+        and creativity, believing that these qualities are essential in driving projects to success
+        and pushing the boundaries of what is possible.
+      </p>
+      <p>
+        Learn more about me at <a href="https://www.chriscelaya.xyz">www.chriscelaya.xyz</a>. I am
+        constantly on the lookout for new challenges and opportunities to apply my skills towards
+        the growth and success of forward-thinking organizations.
+      </p>
     </article>
   </Main>
 );
