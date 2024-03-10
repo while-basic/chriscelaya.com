728ae1b4ddc8fc140a8c3d610a4ee905a6a61256
while-basic <c_celaya@icloud.com>
2024-03-09 17:51:02 -0700

    added images to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,37 +4,35 @@ const data = [
     title: 'MedChat',
     subtitle: '2023 - Artificial Intelligence',
     link: '',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    image: '/images/projects/medchat.webp',
+    date: '2021-11-01',
     desc:
-      'Built for a templates impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'MedChat is a cutting-edge web application that leverages the power of Llama2, a state-of-the-art large language model, to provide accurate and personalized patient diagnoses.'
+      + 'This innovative platform allows users to input their symptoms, medical history, and other relevant information, which is then processed by the Llama2 model.'
+      + 'MedChat\'s user-friendly interface enables seamless interaction with the Llama2 model, streamlining the diagnostic process and reducing the time and effort required for healthcare professionals to evaluate and diagnose patients.',
   },
   {
     title: 'MIDI Saber',
     subtitle: '2021 - Virtual Reality',
     link: 'https://www.youtube.com/watch?v=fW6c3pb60kE',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2021-03-08',
+    image: '/images/projects/midi-saber.webp',
+    date: '2015-06-01',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'MidiSaber is an innovative program that bridges the gap between music production and virtual reality game development. Designed as an interface for the popular virtual reality game "Beat Saber," MidiSaber allows users to create and customize their own levels by repurposing a MIDI controller as a development tool.'
+      + 'At its core, MidiSaber harnesses the power of MIDI technology, enabling users to map notes, rhythms, and patterns from their MIDI controller to the gameplay mechanics of Beat Saber. This seamless integration transforms the act of composing music into a dynamic and immersive experience, where each note played on the MIDI controller corresponds to the generation of new obstacles, challenges, and rhythmic patterns within the virtual reality environment.'
+      + 'With MidiSaber, composers and game developers can bring their musical visions to life in a visually stunning and interactive manner. The program\'s user-friendly interface and intuitive controls make it accessible to both experienced musicians and newcomers alike, allowing them to craft unique and engaging Beat Saber levels tailored to their creative preferences.'
+      + 'Furthermore, MidiSaber offers a wide range of customization options, allowing users to fine-tune the difficulty level, visual effects, and overall aesthetic of their creations. This flexibility ensures that each level is a unique and challenging experience, catering to players of varying skill levels and musical tastes.',
   },
   {
     title: 'Law-GPT',
     subtitle: '2023 - Artificial Intelligence',
     link: 'https://www.youtube.com/watch?v=19iWJPfot6k&t=6s',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    image: '/images/projects/law-gpt.webp',
+    date: '2021-08-01',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'LawGPT is a groundbreaking Apple Watch application that puts legal expertise at your fingertips, powered by the cutting-edge language model from OpenAI. This innovative app is designed to provide users with on-the-go access to accurate and reliable legal information, empowering them to make informed decisions without the immediate need for a lawyer.'
+      + 'With LawGPT, users can simply speak into their Apple Watch, asking legal questions or seeking guidance on various legal matters. The app seamlessly integrates with OpenAI\'s advanced natural language processing capabilities, allowing it to understand and interpret the user\'s queries with remarkable accuracy.'
+      + 'While LawGPT is not a substitute for professional legal counsel, it serves as a valuable resource for individuals seeking quick and reliable legal information on-the-go. Whether you\'re a business professional navigating contractual obligations, a tenant seeking to understand your rights, or simply someone curious about legal matters, LawGPT empowers you with the knowledge you need, right at your wrist.',
   },
 ];
 
