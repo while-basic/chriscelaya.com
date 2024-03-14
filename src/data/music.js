762a9e4fbef4f4b8f23031b7d9950e6b97fea63d
while-basic <c_celaya@icloud.com>
2024-03-14 02:48:54 -0600

    added default qrcode to images as placeholders

--- src/data/music.js
+++ src/data/music.js
@@ -6,14 +6,44 @@ const data = [
     image: '/images/projects/qrcode.webp',
     date: '2023-12-01',
     desc:
-      'Clouds is a track created by Robert Paz and C-Cell (that\'s me)'
+      'Clouds is a track created by Robert Paz and C-Cell'
+      + '',
+  },
+  {
+    title: 'Johnny Quest 3023',
+    subtitle: '',
+    link: '',
+    image: '/images/projects/qrcode.webp',
+    date: '2023-11-01',
+    desc:
+      ''
+      + '',
+  },
+  {
+    title: 'Blossom',
+    subtitle: '',
+    link: '',
+    image: '/images/projects/qrcode.webp',
+    date: '2023-11-01',
+    desc:
+      ''
+      + '',
+  },
+  {
+    title: 'Creep',
+    subtitle: '',
+    link: '',
+    image: '/images/projects/qrcode.webp',
+    date: '2023-12-01',
+    desc:
+      ''
       + '',
   },
   {
     title: '',
     subtitle: '',
     link: '',
-    image: '/images/projects/.webp',
+    image: '/images/projects/qrcode.webp',
     date: '',
     desc:
       ''
@@ -23,37 +53,7 @@ const data = [
     title: '',
     subtitle: '',
     link: '',
-    image: '/images/projects/.webp',
-    date: '',
-    desc:
-      ''
-      + '',
-  },
-  {
-    title: '',
-    subtitle: '',
-    link: '',
-    image: '/images/projects/.webp',
-    date: '',
-    desc:
-      ''
-      + '',
-  },
-  {
-    title: '',
-    subtitle: '',
-    link: '',
-    image: '/images/projects/.webp',
-    date: '',
-    desc:
-      ''
-      + '',
-  },
-  {
-    title: '',
-    subtitle: '',
-    link: '',
-    image: '/images/projects/.webp',
+    image: '/images/projects/qrcode.webp',
     date: '',
     desc:
       ''
