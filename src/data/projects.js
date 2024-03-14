7644a7aa8af529291c2eb74ba80aad706ee21af0
while-basic <c_celaya@icloud.com>
2024-03-13 19:35:42 -0600

    spelling

--- src/data/projects.js
+++ src/data/projects.js
@@ -16,10 +16,10 @@ const data = [
     image: '/images/projects/midi-saber.webp',
     date: '2015-06-01',
     desc:
-      'MidiSaber is an innovative program that bridges the gap between music production and virtual reality game development. Designed as an interface for the popular virtual reality game "Beat Saber," MidiSaber allows users to create and customize their own levels by repurposing a MIDI controller as a development tool.'
-      + 'At its core, MidiSaber harnesses the power of MIDI technology, enabling users to map notes, rhythms, and patterns from their MIDI controller to the gameplay mechanics of Beat Saber. This seamless integration transforms the act of composing music into a dynamic and immersive experience, where each note played on the MIDI controller corresponds to the generation of new obstacles, challenges, and rhythmic patterns within the virtual reality environment.'
+      'Midi Saber is an innovative program that bridges the gap between music production and virtual reality game development. Designed as an interface for the popular virtual reality game "Beat Saber," MidiSaber allows users to create and customize their own levels by repurposing a MIDI controller as a development tool.'
+      + 'At its core, Midi Saber harnesses the power of MIDI technology, enabling users to map notes, rhythms, and patterns from their MIDI controller to the gameplay mechanics of Beat Saber. This seamless integration transforms the act of composing music into a dynamic and immersive experience, where each note played on the MIDI controller corresponds to the generation of new obstacles, challenges, and rhythmic patterns within the virtual reality environment.'
       + 'With MidiSaber, composers and game developers can bring their musical visions to life in a visually stunning and interactive manner. The program\'s user-friendly interface and intuitive controls make it accessible to both experienced musicians and newcomers alike, allowing them to craft unique and engaging Beat Saber levels tailored to their creative preferences.'
-      + 'Furthermore, MidiSaber offers a wide range of customization options, allowing users to fine-tune the difficulty level, visual effects, and overall aesthetic of their creations. This flexibility ensures that each level is a unique and challenging experience, catering to players of varying skill levels and musical tastes.',
+      + 'Furthermore, Midi Saber offers a wide range of customization options, allowing users to fine-tune the difficulty level, visual effects, and overall aesthetic of their creations. This flexibility ensures that each level is a unique and challenging experience, catering to players of varying skill levels and musical tastes.',
   },
   {
     title: 'Law-GPT',
