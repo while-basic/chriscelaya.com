59599dc628dc2adfbd05a6813c2bb3966f2d0c86
while-basic <c_celaya@icloud.com>
2024-11-25 07:59:15 -0700

    removed skills section

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,21 +3,18 @@ import { Link } from 'react-router-dom';
 import Main from '../layouts/Main';
 import Education from '../components/Resume/Education';
 import Experience from '../components/Resume/Experience';
-import Skills from '../components/Resume/Skills';
 import Courses from '../components/Resume/Courses';
 import References from '../components/Resume/References';
 
 import courses from '../data/resume/courses';
 import degrees from '../data/resume/degrees';
 import work from '../data/resume/work';
-import { skills, categories } from '../data/resume/skills';
 
 import './Resume.scss';
 
 // Define section components outside of the render function
 const sections = {
   Experience: <Experience data={work} />,
-  Skills: <Skills skills={skills} categories={categories} />,
   Education: <Education data={degrees} />,
   Courses: <Courses data={courses} />,
   References: <References />,
