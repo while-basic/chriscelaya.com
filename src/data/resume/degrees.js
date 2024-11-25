052ede0357d619ac8b57897aac52b34613b06c9e
while-basic <c_celaya@icloud.com>
2024-11-25 03:35:15 -0700

    updated degrees

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,19 +1,19 @@
 const degrees = [
   {
     school: 'El Paso Community College',
-    degree: 'A.S. Mechanical Engineering, Electrical Engineering, Computer Science',
+    degree: 'Studied Mechanical Engineering, Electrical Engineering, & Computer Science',
     link: 'https://www.epcc.edu/',
     year: 2023,
   },
   {
     school: 'University of Texas at El Paso',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
+    degree: 'Studied Electrical Engineering & Computer Engineering',
     link: 'https://www.utep.edu/',
     year: 2018,
   },
   {
     school: 'University of Texas at Austin',
-    degree: 'B.S. Accounting & Finance',
+    degree: 'Studied Accounting & Finance',
     link: 'https://www.utexas.edu/',
     year: 2015,
   },
