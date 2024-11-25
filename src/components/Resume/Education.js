7fb7ce9951f86070ae0e65365c2c99da91e1e2fb
while-basic <c_celaya@icloud.com>
2024-11-25 08:03:27 -0700

    removed extra education header

--- src/components/Resume/Education.js
+++ src/components/Resume/Education.js
@@ -6,9 +6,6 @@ import Degree from './Education/Degree';
 const Education = ({ data }) => (
   <div className="education">
     <div className="link-to" id="education" />
-    <div className="title">
-      <h3>Education</h3>
-    </div>
     {data.map((degree) => (
       <Degree
         data={degree}
