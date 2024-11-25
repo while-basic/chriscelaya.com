51d392b82825dc25dd1d30567f1e6042ba848351
while-basic <c_celaya@icloud.com>
2024-11-25 07:56:30 -0700

    removed extra heading, changed colors to let's connect.

--- src/components/Resume/Experience.js
+++ src/components/Resume/Experience.js
@@ -6,9 +6,6 @@ import Job from './Experience/Job';
 const Experience = ({ data }) => (
   <div className="experience">
     <div className="link-to" id="experience" />
-    <div className="title">
-      <h3>Experience</h3>
-    </div>
     {data.map((job) => (
       <Job
         data={job}
