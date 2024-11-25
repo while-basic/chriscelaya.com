4b4fedf4ce6d48a4f46db22563a1112883210de4
while-basic <c_celaya@icloud.com>
2024-11-25 07:16:39 -0700

    grammar

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,7 +6,7 @@ const PageNotFound = () => (
   <HelmetProvider>
     <div className="not-found">
       <Helmet title="404 Not Found">
-        <meta name="description" content="The content you are looking for cannot be found." />
+        <meta name="description" content="Uh, Oh...the content you are looking for cannot be found." />
       </Helmet>
       <h1>Page Not Found</h1>
       <p>Return <Link to="/">home</Link>.</p>
