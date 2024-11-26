4cab6d4ef9f358719c8d34433fcaad4214e3182d
while-basic <c_celaya@icloud.com>
2024-11-26 05:55:31 -0700

    added buy me a coffee link

--- src/data/contact.js
+++ src/data/contact.js
@@ -5,6 +5,7 @@ import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons/faCoffee';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
@@ -33,6 +34,11 @@ const data = [
     link: 'mailto:chris@chriscelaya.xyz',
     label: 'Email',
     icon: faEnvelope,
+  },
+  {
+    link: 'https://www.buymeacoffee.com/chriscelaya',
+    label: 'Buy Me a Coffee',
+    icon: faCoffee,
   },
 ];
 
